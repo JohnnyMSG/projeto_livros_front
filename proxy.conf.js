f94ef742b0775bb3fb0315e8ecc3d00eb584feb0
Johnny <johnnymsgouveia@gmail.com>
2025-06-09 18:41:14 -0300

    Path alterado para projeto-livros-back.onrender.com

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
   {
     context: ['/api'],
-    target: 'http://localhost:8090/',
+    target: 'http://projeto-livros-back.onrender.com/',
     secure: false,
     logLevel: 'debug',
     pathRewrite: { '^/api': '' }
